a60a9742840820f37190207697613741fbb3b356
Victordev32 <vkn109773@gmail.com>
2026-07-22 13:43:19 +0300

    created static folder on project directory,created js folder and script.js,created menu button,made sidebar responsive as a menu using js

---
+++ static/js/script.js
@@ -0,0 +1,11 @@
+
+let menu_btn=document.getElementById("menu-btn")
+let menu=document.getElementById("menu")
+document.addEventListener("DOMContentLoaded",()=>{
+  
+})
+
+menu_btn.onclick=()=>{
+  
+  menu.classList.toggle("hidden")
+}
